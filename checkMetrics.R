d357c396664ea7cb9ffa845c2c484c0ae9bc746c
alex-font-mercadie <alex.font@estudiantat.upc.edu>
2025-11-15 10:23:56 +0100

    Add files via upload

--- checkMetrics.R
+++ checkMetrics.R
@@ -11,19 +11,7 @@ for (fl in files) {
   lower_bound_k = all(4*metrics$n - 6 <= metrics$nk2)
   upper_bound_k = all(metrics$k2 <= metrics$n - 1)
   
-  print(paste0(lang, " ", lower_bound_k, " ", upper_bound_k))
-  
-  if(!lower_bound_k) {
-    error_index <- which((4*metrics$n - 6 > metrics$nk2))
-    print(metrics[error_index,])
-    print(metrics[error_index,]$nk2 - (4*metrics[error_index,]$n - 6))
-  }
-  
-  if(!upper_bound_k) {
-    error_index <- which(metrics$k2 > metrics$n - 1)
-    print(metrics[error_index,])
-    print(metrics[error_index,]$k2 - metrics[error_index,]$n - 1)
-  }
+  print(paste0(lang, ": ", (lower_bound_k & upper_bound_k)))
 }
 
 
